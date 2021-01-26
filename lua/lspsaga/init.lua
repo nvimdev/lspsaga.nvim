0b22c10fcd2d3f639bf5ec068914174254d4193f
stephen <glepnir@gopherhub.org>
2021-01-26 08:44:57 +0800

    add option to disable diagnostic handler overwrite

--- lua/lspsaga/init.lua
+++ lua/lspsaga/init.lua
@@ -13,6 +13,7 @@ saga.config_values = {
   definition_preview_icon = '  ',
   -- 1: thin border | 2: rounded border | 3: thick border
   border_style = 1
+  use_saga_diagnostic_handler = 1
 }
 
 function saga.extend_config(opts)
