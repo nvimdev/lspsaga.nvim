e9eff8f85375a9f963b68435170fd2ab9ab2e3a1
bitcoder <glepnir@gopherhub.org>
2021-02-08 19:19:54 +0800

    close #81

--- lua/lspsaga/hover.lua
+++ lua/lspsaga/hover.lua
@@ -2,22 +2,35 @@ local api,lsp,util = vim.api,vim.lsp,vim.lsp.util
 local window = require('lspsaga.window')
 local config = require('lspsaga').config_values
 local action = require('lspsaga.action')
+local npcall = vim.F.npcall
 local hover = {}
 
-local call_back = function (_,method,result)
-    vim.lsp.util.focusable_float(method, function()
-        if not (result and result.contents) then return end
-        local markdown_lines = lsp.util.convert_input_to_markdown_lines(result.contents)
-        markdown_lines = lsp.util.trim_empty_lines(markdown_lines)
-        if vim.tbl_isempty(markdown_lines) then return end
-        window.nvim_win_try_close()
-        local bufnr,contents_winid,_,border_winid = window.fancy_floating_markdown(markdown_lines, {
-          border_style = config.border_style,
-        })
+local function focusable_float(unique_name, fn)
+  if npcall(api.nvim_win_get_var, 0, unique_name) then
+    return api.nvim_command("wincmd p")
+  end
+  local bufnr = api.nvim_get_current_buf()
+  local pbufnr, pwinnr = fn()
+  if pbufnr then
+    api.nvim_win_set_var(pwinnr, unique_name, bufnr)
+    return pbufnr, pwinnr
+  end
+end
 
-        lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, contents_winid)
-        lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, border_winid)
-        return bufnr,contents_winid
+local call_back = function (_,method,result)
+    focusable_float(method, function()
+      if not (result and result.contents) then return end
+      local markdown_lines = lsp.util.convert_input_to_markdown_lines(result.contents)
+      markdown_lines = lsp.util.trim_empty_lines(markdown_lines)
+      if vim.tbl_isempty(markdown_lines) then return end
+      window.nvim_win_try_close()
+      local bufnr,contents_winid,_,border_winid = window.fancy_floating_markdown(markdown_lines, {
+        border_style = config.border_style,
+      })
+
+      lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, contents_winid)
+      lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, border_winid)
+      return bufnr,contents_winid
     end)
 end
 
