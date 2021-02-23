0801c01408b67c507bc768246a37e004a1566126
Alexander Popov <alex.popov.tech@gmail.com>
2021-02-24 00:54:19 +0200

    search just for buf_active servers

--- lua/lspsaga/signaturehelp.lua
+++ lua/lspsaga/signaturehelp.lua
@@ -19,7 +19,7 @@ end
 local function check_server_support_signaturehelp()
   local active,msg = libs.check_lsp_active()
   if not active then print(msg) return end
-  local clients = vim.lsp.get_active_clients()
+  local clients = vim.lsp.buf_get_clients()
   for _,client in ipairs(clients) do
     if client.resolved_capabilities.signature_help == true then
       return true
