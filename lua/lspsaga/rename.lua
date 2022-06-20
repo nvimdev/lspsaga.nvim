a7ce7771915393ce8cb4319e49e779867207f0da
Raphael <glephunter@gmail.com>
2022-06-20 20:47:18 +0800

    fix rename

--- lua/lspsaga/rename.lua
+++ lua/lspsaga/rename.lua
@@ -1,4 +1,4 @@
-local lsp,util,api = vim.lsp,vim.lsp.util,vim.api
+local util,api = vim.lsp.util,vim.api
 local window = require('lspsaga.window')
 local config = require('lspsaga').config_values
 local libs = require('lspsaga.libs')
@@ -43,7 +43,7 @@ end
 
 local lsp_rename = function()
   local active,msg = libs.check_lsp_active()
-  if not active then print(msg) return end
+  if not active then vim.notify(msg) return end
   -- if exist a rename float win close it.
   close_rename_win()
   pos[1],pos[2] = vim.fn.line('.'),vim.fn.col('.')
@@ -82,79 +82,12 @@ local lsp_rename = function()
   apply_action_keys(bufnr)
 end
 
-local method = 'textDocument/rename'
-
 local do_rename = function(options)
   options = options or {}
   local prompt_prefix = get_prompt_prefix()
   local new_name = vim.trim(vim.fn.getline('.'):sub(#prompt_prefix+1,-1))
   close_rename_win()
-
-  local bufnr = api.nvim_get_current_buf()
-
-  local clients = vim.lsp.get_active_clients({
-    bufnr = bufnr,
-    name = options.name,
-  })
-
-  if options.filter then
-    clients = vim.tbl_filter(options.filter, clients)
-  end
-
-  -- Clients must at least support rename, prepareRename is optional
-  clients = vim.tbl_filter(function(client)
-    return client.supports_method(method)
-  end, clients)
-
-  if #clients == 0 then
-    vim.notify('[LSP] Rename, no matching language servers with rename capability.')
-  end
-
-  local try_use_client
-  try_use_client = function(idx,client)
-    local win = api.nvim_get_current_win()
-
-    if not client then
-      return
-    end
-
-    local function rename(name)
-      local params = util.make_position_params(win, client.offset_encoding)
-      params.newName = name
-      local handler = client.handlers[method] or vim.lsp.handlers[method]
-      client.request(method, params, function(...)
-        handler(...)
-        try_use_client(next(clients, idx))
-      end, bufnr)
-    end
-
-    if client.supports_method('textDocument/prepareRename') then
-      local params = util.make_position_params(win, client.offset_encoding)
-      client.request('textDocument/prepareRename', params, function(err, result)
-        if err or result == nil then
-          if next(clients, idx) then
-            try_use_client(next(clients, idx))
-          else
-            local msg = err and ('Error on prepareRename: ' .. (err.message or '')) or 'Nothing to rename'
-            vim.notify(msg, vim.log.levels.INFO)
-          end
-          return
-        end
-
-        if new_name then
-          rename(new_name)
-          return
-        end
-      end)
-    else
-      assert(client.supports_method(method), 'Client must support textDocument/rename')
-      if new_name then
-        rename(new_name)
-        return
-      end
-    end
-  end
-  try_use_client(next(clients))
+  vim.lsp.buf.rename(new_name)
 end
 
 return {
