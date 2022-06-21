2c08e02cf3a8c1937462345a5ca871fb8cb2c4a9
Raphael <glephunter@gmail.com>
2022-06-21 18:05:55 +0800

    fix rename bug

--- lua/lspsaga/rename.lua
+++ lua/lspsaga/rename.lua
@@ -17,8 +17,6 @@ local close_rename_win = function()
   local has,winid = pcall(api.nvim_win_get_var,0,unique_name)
   if has then
     window.nvim_close_valid_window(winid)
-    api.nvim_win_set_cursor(0,pos)
-    pos = {}
   end
 end
 
@@ -29,7 +27,7 @@ local apply_action_keys = function(bufnr)
   local rhs_of_exec = [[<cmd>lua require('lspsaga.rename').do_rename()<CR>]]
   local opts = {nowait = true,silent = true,noremap = true}
 
-  api.nvim_buf_set_keymap(bufnr,'i',exec_key,rhs_of_exec,opts)
+--   api.nvim_buf_set_keymap(bufnr,'i',exec_key,rhs_of_exec,opts)
 
   if type(quit_key) == "table" then
     for _,k in ipairs(quit_key) do
@@ -44,9 +42,11 @@ end
 local lsp_rename = function()
   local active,msg = libs.check_lsp_active()
   if not active then vim.notify(msg) return end
+  local current_buf = api.nvim_get_current_buf()
+  local current_win = api.nvim_get_current_win()
   -- if exist a rename float win close it.
   close_rename_win()
-  pos[1],pos[2] = vim.fn.line('.'),vim.fn.col('.')
+  pos = api.nvim_win_get_cursor(current_win)
 
   local opts = {
     height = 1,
@@ -69,6 +69,17 @@ local lsp_rename = function()
   api.nvim_buf_set_option(bufnr,'buftype','prompt')
   vim.fn.prompt_setprompt(bufnr, prompt_prefix)
   api.nvim_buf_add_highlight(bufnr, saga_rename_prompt_prefix, 'LspSagaRenamePromptPrefix', 0, 0, #prompt_prefix)
+  local options = {
+    bufnr = current_buf
+  }
+  vim.fn.prompt_setcallback(bufnr,function(new_name)
+    close_rename_win()
+    api.nvim_win_set_cursor(current_win,pos)
+    vim.lsp.buf.rename(new_name,options)
+    api.nvim_win_set_cursor(current_win,{pos[1],pos[2]+1})
+    pos = {}
+  end)
+
   vim.cmd [[startinsert!]]
   api.nvim_win_set_var(0,unique_name,winid)
   api.nvim_create_autocmd('QuitPre',{
@@ -82,16 +93,7 @@ local lsp_rename = function()
   apply_action_keys(bufnr)
 end
 
-local do_rename = function(options)
-  options = options or {}
-  local prompt_prefix = get_prompt_prefix()
-  local new_name = vim.trim(vim.fn.getline('.'):sub(#prompt_prefix+1,-1))
-  close_rename_win()
-  vim.lsp.buf.rename(new_name)
-end
-
 return {
   lsp_rename = lsp_rename,
-  do_rename = do_rename,
-  close_rename_win = close_rename_win
+  close_rename_win = close_rename_win,
 }
