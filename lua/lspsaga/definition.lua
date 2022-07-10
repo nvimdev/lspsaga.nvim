984265061b69d4f71b05d377cce35678078a3193
Kai Zhang <kaizhang16@qq.com>
2022-07-10 16:31:39 +0800

    fix split_by_pathsep

--- lua/lspsaga/definition.lua
+++ lua/lspsaga/definition.lua
@@ -41,9 +41,9 @@ function def.preview_definition(timeout_ms)
     if link:find(root_dir, 1, true) then
       short_name = link:sub(root_dir:len() + 2)
     else
-      local _split = vim.split(link,path_sep)
+      local _split = vim.split(link, path_sep)
       if #_split > 5 then
-        short_name = table.concat(_split,path_sep,#_split - 2,#_split)
+        short_name = table.concat(_split, path_sep, #_split - 2, #_split)
       end
     end
 
