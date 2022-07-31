c8bb855fc4ad2cfc9b6ab7bc5e4f0e0a063cc400
Syncrack <stghjy@gmail.com>
2022-07-31 10:53:31 +0800

    fix(lightbulb): use passed in bufnr (#388)
    the current buffer may have been changed during coroutine running and that causes error.

--- lua/lspsaga/lightbulb.lua
+++ lua/lspsaga/lightbulb.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 local libs = require('lspsaga.libs')
 local config = require('lspsaga').config_values
-local method = 'textDocument/codeAction'
+local code_action_method = 'textDocument/codeAction'
 local lb = {}
 
 local SIGN_GROUP = 'sagalightbulb'
@@ -11,23 +11,23 @@ if vim.tbl_isempty(vim.fn.sign_getdefined(SIGN_NAME)) then
   vim.fn.sign_define(SIGN_NAME, { text = config.code_action_icon, texthl = 'LspSagaLightBulb' })
 end
 
-local function check_server_support_codeaction()
-  local clients = vim.lsp.buf_get_clients()
+local function check_server_support_codeaction(bufnr)
+  local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
   for _, client in pairs(clients) do
-    if client.supports_method('textDocument/codeAction') then
+    if client.supports_method(code_action_method) then
       return true
     end
   end
   return false
 end
 
-local function _update_virtual_text(line)
+local function _update_virtual_text(bufnr, line)
   local namespace = api.nvim_create_namespace('sagalightbulb')
-  api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+  api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
 
   if line then
     local icon_with_indent = '  ' .. config.code_action_icon
-    api.nvim_buf_set_extmark(0, namespace, line, -1, {
+    api.nvim_buf_set_extmark(bufnr, namespace, line, -1, {
       virt_text = { { icon_with_indent, 'LspSagaLightBulb' } },
       virt_text_pos = 'overlay',
       hl_mode = 'combine',
@@ -35,13 +35,12 @@ local function _update_virtual_text(line)
   end
 end
 
-local function _update_sign(line)
-  local current_buf = api.nvim_get_current_buf()
+local function _update_sign(bufnr, line)
   if vim.w.lightbulb_line == 0 then
     vim.w.lightbulb_line = 1
   end
   if vim.w.lightbulb_line ~= 0 then
-    vim.fn.sign_unplace(SIGN_GROUP, { id = vim.w.lightbulb_line, buffer = current_buf })
+    vim.fn.sign_unplace(SIGN_GROUP, { id = vim.w.lightbulb_line, buffer = bufnr })
   end
 
   if line then
@@ -49,43 +48,28 @@ local function _update_sign(line)
       line,
       SIGN_GROUP,
       SIGN_NAME,
-      current_buf,
+      bufnr,
       { lnum = line + 1, priority = config.code_action_lightbulb.sign_priority }
     )
     vim.w.lightbulb_line = line
   end
 end
 
-local function render_action_virtual_text(line, diagnostics, actions)
-  if next(actions) == nil and next(diagnostics) == nil then
+local function render_action_virtual_text(bufnr, line, diagnostics, actions)
+  if next(actions) == nil then
     if config.code_action_lightbulb.virtual_text then
-      _update_virtual_text(nil)
+      _update_virtual_text(bufnr, nil)
     end
     if config.code_action_lightbulb.sign then
-      _update_sign(nil)
-    end
-  elseif next(actions) ~= nil and next(diagnostics) == nil then
-    if config.code_action_lightbulb.sign then
-      _update_sign(line)
-    end
-
-    if config.code_action_lightbulb.virtual_text then
-      _update_virtual_text(line)
-    end
-  elseif next(actions) ~= nil and next(diagnostics) ~= nil then
-    if config.code_action_lightbulb.sign then
-      _update_sign(line)
-    end
-
-    if config.code_action_lightbulb.virtual_text then
-      _update_virtual_text(line)
+      _update_sign(bufnr, nil)
     end
   else
-    if config.code_action_lightbulb.virtual_text then
-      _update_virtual_text(nil)
-    end
     if config.code_action_lightbulb.sign then
-      _update_sign(nil)
+      _update_sign(bufnr, line)
+    end
+
+    if config.code_action_lightbulb.virtual_text then
+      _update_virtual_text(bufnr, line)
     end
   end
 end
@@ -100,7 +84,7 @@ local send_request = coroutine.create(function()
     local params = vim.lsp.util.make_range_params()
     params.context = context
     local line = params.range.start.line
-    vim.lsp.buf_request_all(current_buf, method, params, function(results)
+    vim.lsp.buf_request_all(current_buf, code_action_method, params, function(results)
       if libs.result_isempty(results) then
         results = { { result = {} } }
       end
@@ -110,7 +94,7 @@ local send_request = coroutine.create(function()
           table.insert(actions, res.result)
         end
       end
-      render_action_virtual_text(line, diagnostics, actions)
+      render_action_virtual_text(current_buf, line, diagnostics, actions)
     end)
     current_buf = coroutine.yield()
   end
@@ -118,21 +102,19 @@ end)
 
 local render_bulb = function()
   local current_buf = api.nvim_get_current_buf()
+  local has_code_action = check_server_support_codeaction(current_buf)
+  if not has_code_action then
+    return
+  end
   coroutine.resume(send_request, current_buf)
 end
 
 function lb.action_lightbulb()
-  local has_code_action = check_server_support_codeaction()
-  if not has_code_action then
-    return
-  end
-
   if not config.code_action_lightbulb.enable_in_insert and vim.fn.mode() == 'i' then
-    _update_sign()
-    _update_virtual_text()
+    _update_sign(0, nil)
+    _update_virtual_text(0, nil)
     return
   end
-
   render_bulb()
 end
 
