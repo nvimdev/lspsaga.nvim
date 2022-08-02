20b8ebec7717c4591a74df0b8fbd234ff92f7d93
Raphael <glephunter@gmail.com>
2022-08-02 20:41:54 +0800

    finder: if file not save do save then do open close #400

--- lua/lspsaga/finder.lua
+++ lua/lspsaga/finder.lua
@@ -605,6 +605,9 @@ function Finder:open_link(action_type)
   end
 
   self:quit_float_window(false)
+  if vim.bo.modified then
+    vim.cmd('write')
+  end
   api.nvim_command(action[action_type] .. self.short_link[current_line].link)
   fn.cursor(self.short_link[current_line].row, self.short_link[current_line].col)
   self:clear_tmp_data()
