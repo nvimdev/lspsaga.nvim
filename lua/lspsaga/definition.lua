c3a978e2a3fb9388b562e302abed8ce18d4bc0bc
Raphael <glephunter@gmail.com>
2022-09-03 16:18:32 +0800

    update

--- lua/lspsaga/definition.lua
+++ lua/lspsaga/definition.lua
@@ -57,21 +57,9 @@ function def:preview_definition()
     if not vim.api.nvim_buf_is_loaded(bufnr) then
       fn.bufload(bufnr)
     end
-    local range = result[1].targetRange or result[1].range
-    local start_line = range.start.line
-
-    local start_char_pos = range.start.character
-
-    local content = vim.api.nvim_buf_get_lines(
-      bufnr,
-      start_line,
-      range['end'].line + 1 + config.max_preview_lines,
-      false
-    )
 
     local prompt = config.definition_preview_icon .. 'File: '
     local quit = config.definition_action_keys.quit
-    content = vim.list_extend({ prompt .. short_name .. ' ' .. quit .. ' to quit', '' }, content)
 
     local opts = {
       relative = 'cursor',
@@ -80,65 +68,59 @@ function def:preview_definition()
     local WIN_WIDTH = api.nvim_get_option('columns')
     local max_width = math.floor(WIN_WIDTH * 0.6)
     local max_height = math.floor(vim.o.lines * 0.6)
-    local width, _ = vim.lsp.util._make_floating_popup_size(content, opts)
 
-    if width > max_width then
-      opts.width = max_width
-    end
-
-    if #content > max_height then
-      opts.height = max_height
-    end
+    opts.width = max_width
+    opts.height = max_height
 
     local content_opts = {
-      contents = content,
+      contents = {},
       filetype = filetype,
       enter = true,
       highlight = 'LspSagaDefPreviewBorder',
     }
 
     self.bufnr, self.winid = window.create_win_with_border(content_opts, opts)
-    if not vim.opt_local.modifiable:get() then
-      vim.opt_local.modifiable = true
-    end
+    vim.opt_local.modifiable = true
+    api.nvim_win_set_buf(0,bufnr)
+    api.nvim_win_set_option(self.winid,'winbar',"")
     --set the initail cursor pos
-    api.nvim_win_set_cursor(self.winid, { 3, start_char_pos })
+    -- api.nvim_win_set_cursor(self.winid, { 3, start_char_pos })
 
-    api.nvim_create_autocmd({ 'CursorMoved', 'InsertEnter', 'BufHidden' }, {
-      buffer = current_buf,
-      once = true,
-      callback = function()
-        if self.winid ~= nil then
-          window.nvim_close_valid_window(self.winid)
-        end
-        self:clear_tmp_data()
-      end,
-      desc = 'Auto close lspsaga definition preview window',
-    })
+    -- api.nvim_create_autocmd({ 'CursorMoved', 'InsertEnter', 'BufHidden' }, {
+    --   buffer = current_buf,
+    --   once = true,
+    --   callback = function()
+    --     if self.winid ~= nil then
+    --       window.nvim_close_valid_window(self.winid)
+    --     end
+    --     self:clear_tmp_data()
+    --   end,
+    --   desc = 'Auto close lspsaga definition preview window',
+    -- })
 
-    vim.keymap.set('n', quit, function()
-      if self.winid and api.nvim_win_is_valid(self.winid) then
-        api.nvim_win_close(self.winid, true)
-      end
-    end, { buffer = self.bufnr })
+    -- vim.keymap.set('n', quit, function()
+    --   if self.winid and api.nvim_win_is_valid(self.winid) then
+    --     api.nvim_win_close(self.winid, true)
+    --   end
+    -- end, { buffer = bufnr })
 
-    api.nvim_buf_add_highlight(self.bufnr, -1, 'DefinitionPreviewIcon', 0, 0, #prompt - 1)
-    api.nvim_buf_add_highlight(
-      self.bufnr,
-      0,
-      'DefinitionPreviewFile',
-      0,
-      #prompt + 1,
-      #prompt + #short_name
-    )
-    api.nvim_buf_add_highlight(
-      self.bufnr,
-      -1,
-      'DefinitionPreviewTip',
-      0,
-      #prompt + #short_name + 2,
-      -1
-    )
+    -- api.nvim_buf_add_highlight(self.bufnr, -1, 'DefinitionPreviewIcon', 0, 0, #prompt - 1)
+    -- api.nvim_buf_add_highlight(
+    --   self.bufnr,
+    --   0,
+    --   'DefinitionPreviewFile',
+    --   0,
+    --   #prompt + 1,
+    --   #prompt + #short_name
+    -- )
+    -- api.nvim_buf_add_highlight(
+    --   self.bufnr,
+    --   -1,
+    --   'DefinitionPreviewTip',
+    --   0,
+    --   #prompt + #short_name + 2,
+    --   -1
+    -- )
   end)
 end
 
