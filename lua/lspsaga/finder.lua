b4018b843fb020777d628095f242926bd5cced29
Raphael <glephunter@gmail.com>
2022-09-16 15:22:11 +0800

    finder: make sure close all preview window close #519

--- lua/lspsaga/finder.lua
+++ lua/lspsaga/finder.lua
@@ -95,7 +95,7 @@ function Finder:loading_bar()
       '█████████▁',
       '██████████',
     },
-    interval = 10,
+    interval = 15,
     timeout = config.finder_request_timeout,
   }
   api.nvim_buf_set_option(spin_buf, 'modifiable', true)
@@ -375,7 +375,7 @@ function Finder:render_finder_result()
     end,
   })
 
-  local events = { 'QuitPre', 'WinLeave' }
+  local events = { 'WinClosed', 'WinLeave' }
 
   api.nvim_create_autocmd(events, {
     group = finder_group,
