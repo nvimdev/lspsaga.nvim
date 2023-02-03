1ee6d695b4b5159ebd08a08cc2850a96292271ca
Maximilian Øystå Lloyd <me@maxlloyd.no>
2023-02-03 13:38:40 +0100

    Ran stylua to format codeaction.lua

--- lua/lspsaga/codeaction.lua
+++ lua/lspsaga/codeaction.lua
@@ -29,10 +29,10 @@ function act:action_callback()
     if client_with_actions[2].title then
       action_title = indent .. index .. '  ' .. client_with_actions[2].title
     end
-		if (config.code_action.show_server_name == true) then
-			local name = vim.lsp.get_client_by_id(client_with_actions[1]).name
-			action_title = action_title .. '  ' .. name
-		end
+    if config.code_action.show_server_name == true then
+      local name = vim.lsp.get_client_by_id(client_with_actions[1]).name
+      action_title = action_title .. '  ' .. name
+    end
     table.insert(contents, action_title)
   end
 
