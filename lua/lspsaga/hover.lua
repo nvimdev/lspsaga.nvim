771fd40a56eb2f89ebae6e53ef6e4c2db37ae405
Raphael <glephunter@gmail.com>
2023-02-09 15:57:49 +0800

    hover: improve markdown parse for brackets

--- lua/lspsaga/hover.lua
+++ lua/lspsaga/hover.lua
@@ -4,6 +4,23 @@ local window = require('lspsaga.window')
 local libs = require('lspsaga.libs')
 local hover = {}
 
+local function query_bracket()
+  local query = [[
+    (shortcut_link
+      (
+        "[" @conceall (#set! conceall "")
+        (link_text) @text (#not-eq? text " ")
+        "]" @concealr (#set! concealr "")
+      )
+    )
+  ]]
+  vim.treesitter.query.set_query('markdown_inline', 'highlights', query)
+end
+
+do
+  query_bracket()
+end
+
 local function has_arg(args, arg)
   local tbl = vim.split(args, '%s')
   if vim.tbl_contains(tbl, arg) then
@@ -26,6 +43,7 @@ function hover:open_floating_preview(res, option_fn)
   for _, line in pairs(content) do
     if line:find('\\') then
       line = line:gsub('\\', '')
+      print(line)
     end
     if line:find('\r') then
       line = line:gsub('\r\n?', ' ')
