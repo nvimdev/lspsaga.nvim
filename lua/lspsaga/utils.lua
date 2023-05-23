70ef3f54ad175d5e7a0bdd671affd45b7cea3222
Tsakiris Tryfon <tr.tsakiris@gmail.com>
2023-05-23 18:37:32 +0300

    use ipairs and rename to rhs,lhs

--- lua/lspsaga/utils.lua
+++ lua/lspsaga/utils.lua
@@ -8,19 +8,19 @@ end
 ---@param buffer number
 ---@param modes string|table<string>
 ---@param keys string|table<string>
----@param action string|function
+---@param rhs string|function
 ---@param opts? table
-function M.map_keys(buffer, modes, keys, action, opts)
+function M.map_keys(buffer, modes, keys, rhs, opts)
   opts = opts or {}
 
-  if type(action) == 'function' then
-    opts.callback = action
-    action = ''
+  if type(rhs) == 'function' then
+    opts.callback = rhs
+    rhs = ''
   end
 
-  for _, mode in pairs(M.as_table(modes)) do
-    for _, key in pairs(M.as_table(keys)) do
-      vim.api.nvim_buf_set_keymap(buffer, mode, key, action, opts)
+  for _, mode in ipairs(M.as_table(modes)) do
+    for _, lhs in ipairs(M.as_table(keys)) do
+      vim.api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
     end
   end
 end
