3bbc6fd78fb7ae5ddd5833d8c2d407910f358bfc
mathew <glepnir@neovim.pro>
2023-06-28 17:52:28 +0800

    update

--- test/util_spec.lua
+++ test/util_spec.lua
@@ -1,8 +1,9 @@
 local helper = require('test.helper')
 local api = vim.api
 local util = require('lspsaga.util')
-local eq = assert.equal
+local eq, is_true = assert.equal, assert.is_true
 
+---util module unit test
 describe('lspsaga util', function()
   local bufnr
   before_each(function()
@@ -10,10 +11,43 @@ describe('lspsaga util', function()
     api.nvim_win_set_buf(0, bufnr)
   end)
 
-  it('util.get_path_info', function()
+  it('util.path_itera', function()
     api.nvim_buf_set_name(bufnr, 'test.lua')
-    local tbl = util.get_path_info(bufnr)
-    eq(1, #tbl)
-    eq('test.lua', tbl[#tbl])
+    local result = {}
+    for part in util.path_itera(bufnr) do
+      result[#result + 1] = part
+    end
+    eq('test.lua', result[1])
+  end)
+
+  it('util.tbl_index', function()
+    local case = { 1, 2, 3, 8 }
+    eq(4, util.tbl_index(case, 8))
+  end)
+
+  it('util.close_win', function()
+    vim.cmd.split()
+    util.close_win(api.nvim_get_current_win())
+    assert.is_true(true, #api.nvim_list_wins() == 1)
+  end)
+
+  it('util.as_table', function()
+    assert.same({ 10 }, util.as_table(10))
+    assert.same({ 10 }, util.as_table({ 10 }))
+  end)
+
+  it('util.map_keys', function()
+    util.map_keys(bufnr, 'n', 'gq', function()
+      return '<Nop>'
+    end)
+    local maps = api.nvim_buf_get_keymap(bufnr, 'n')
+    local created = false
+    for _, item in ipairs(maps) do
+      if item.lhs == 'gq' then
+        created = true
+        break
+      end
+    end
+    is_true(true, created)
   end)
 end)
