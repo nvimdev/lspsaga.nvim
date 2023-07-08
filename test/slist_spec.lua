911e58bd928729904a4f4e681cdf8bd654d15639
mathew <glepnir@neovim.pro>
2023-07-08 20:44:26 +0800

    update

--- test/slist_spec.lua
+++ test/slist_spec.lua
@@ -38,6 +38,45 @@ describe('single linked list module ', function()
     }, list)
   end)
 
+  it('can insert node', function()
+    local node = slist.find_node(list, 1)
+    local tmp = {
+      name = 'test_insert',
+      winline = -1,
+      range = {
+        start = {
+          line = 2,
+          character = 2,
+        },
+      },
+    }
+    slist.insert_node(node, tmp)
+    same({
+      value = {
+        name = 'test_insert',
+        winline = -1,
+        range = {
+          start = {
+            line = 2,
+            character = 2,
+          },
+        },
+      },
+      next = {
+        value = {
+          name = 'test2',
+          winline = 2,
+          range = {
+            start = {
+              line = 2,
+              character = 2,
+            },
+          },
+        },
+      },
+    }, slist.find_node(list, -1))
+  end)
+
   it('can find node', function()
     local node = slist.find_node(list, 2)
     same({
@@ -52,5 +91,13 @@ describe('single linked list module ', function()
         },
       },
     }, node)
+  end)
+
+  it('can map all node', function()
+    local result = {}
+    slist.list_map(list, function(node)
+      result[#result + 1] = node.value.name
+    end)
+    same({ 'test', 'test_insert', 'test2' }, result)
   end)
 end)
