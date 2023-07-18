8008c9428925a286a467d5faf0938acdca785c64
mathew <glepnir@neovim.pro>
2023-07-18 14:45:31 +0800

    update health check api close #1184

--- lua/lspsaga/health.lua
+++ lua/lspsaga/health.lua
@@ -1,25 +1,29 @@
 local fn, health, api = vim.fn, vim.health, vim.api
 local M = {}
+local err = vim.version().minor >= 10 and health.error or health.report_error
+local warn = vim.version().minor >= 10 and health.warn or health.report_warn
+local ok = vim.version().minor >= 10 and health.ok or health.report_ok
+local start = vim.version().minor >= 10 and health.start or health.report_start
 
 local function treesitter_check()
   if fn.executable('tree-sitter') == 0 then
-    health.report_warn('`tree-sitter` executable not found ')
+    warn('`tree-sitter` executable not found ')
   else
-    health.report_ok('`tree-sitter` found ')
+    ok('`tree-sitter` found ')
   end
 
   for _, parser in ipairs({ 'markdown', 'markdown_inline' }) do
     local installed = #api.nvim_get_runtime_file('parser/' .. parser .. '.so', false)
     if installed == 0 then
-      health.report_error('tree-sitter `' .. parser .. '` parser not found')
+      err('tree-sitter `' .. parser .. '` parser not found')
     else
-      health.report_ok('tree-sitter `' .. parser .. '` parser found')
+      ok('tree-sitter `' .. parser .. '` parser found')
     end
   end
 end
 
 M.check = function()
-  health.report_start('Lspsaga.nvim report')
+  start('Lspsaga.nvim report')
   treesitter_check()
 end
 
