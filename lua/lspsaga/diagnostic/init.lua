316c41f4cb0611717115af3be8a9dea9a402ec81
glepnir <glephunter@gmail.com>
2023-09-01 11:41:29 +0800

    check end_col close #1270

--- lua/lspsaga/diagnostic/init.lua
+++ lua/lspsaga/diagnostic/init.lua
@@ -52,7 +52,7 @@ function diag:get_diagnostic(opt)
   if opt.cursor then
     local res = {}
     for _, v in pairs(entrys) do
-      if v.col <= col and v.end_col > col then
+      if v.col <= col and (v.end_col and v.end_col > col or true) then
         res[#res + 1] = v
       end
     end
