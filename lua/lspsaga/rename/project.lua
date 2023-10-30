2d9d5558f0b26bcf9f2be55d1cc2f9694437d5fd
glepnir <glephunter@gmail.com>
2023-10-30 08:07:55 +0800

    fix project rename close #1338

--- lua/lspsaga/rename/project.lua
+++ lua/lspsaga/rename/project.lua
@@ -122,11 +122,11 @@ end
 
 local function render(chunks, new_name)
   local result = decode(chunks)
-  local bufnr, winid = create_win()
   local line = 1
   if vim.tbl_isempty(result) then
     return
   end
+  local bufnr, winid = create_win()
 
   for fname, item in pairs(result) do
     fname = util.path_sub(fname, get_root_dir())
